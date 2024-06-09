e72ade4b6e9714be01f449dd60137923bed10c6e
unknown <shilpas2101@gmail.com>
2024-06-09 12:12:15 +0530

    uploaded

---
+++ src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Search = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">Username</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-danger">Search</button>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Search
